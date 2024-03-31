96328d7a33179fdc133e9d9c9ccb541bb9cbe009
Eli Zevin <dizzyjaguar@gmail.com>
2024-03-31 08:38:15 -0700

    update header title and add about link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,17 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0 justify-between w-full">
       <h2 className="text-lg font-bold tracking-tight">
-        <Link to="/">Waku starter</Link>
+        <Link to="/">
+          Waku <span className="text-red-400">Fruit</span> Shop
+        </Link>
       </h2>
+      <div>
+        <h2 className="text-lg font-bold tracking-tight">
+          <Link to="/about">About</Link>
+        </h2>
+      </div>
     </header>
   );
 };
