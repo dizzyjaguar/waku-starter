26fb840bd7911f07121b5770f005d1d9d36d7ef6
Eli Zevin <dizzyjaguar@gmail.com>
2024-04-01 11:47:21 -0700

    clean up home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,11 @@
-import { Link } from 'waku';
-
-import { Counter } from '../components/counter.js';
-
 export default async function HomePage() {
-  const data = await getData();
-
   return (
     <div>
-      <title>{data.title}</title>
-      <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
-      <p>{data.body}</p>
-      <Counter />
-      <Link to="/about" className="mt-4 inline-block underline">
-        About page
-      </Link>
+      <title>Waku Fruit Shop</title>
+      <h1 className="text-4xl font-bold tracking-tight">Waku Fruit Shop</h1>
     </div>
   );
 }
-
-const getData = async () => {
-  const data = {
-    title: 'Waku',
-    headline: 'Waku',
-    body: 'Hello world!',
-  };
-
-  return data;
-};
 
 export const getConfig = async () => {
   return {
